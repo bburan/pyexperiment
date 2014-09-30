2e58f57e99a1d9106505d195689338bece5e46df
Brad Buran <buran@ohsu.edu>
2014-09-30 15:52:19 -0700

    BUG: Ensure EpochChannelPlot updates on resize

--- experiment/plots/epoch_channel_plot.py
+++ experiment/plots/epoch_channel_plot.py
@@ -1,6 +1,9 @@
-from traits.api import Property, Int, cached_property, Instance, Float
 import numpy as np
-from base_channel_plot import BaseChannelPlot
+
+from traits.api import (Property, Int, cached_property, Instance, Float,
+                        on_trait_change)
+
+from .base_channel_plot import BaseChannelPlot
 
 
 class EpochChannelPlot(BaseChannelPlot):
@@ -11,7 +14,6 @@ class EpochChannelPlot(BaseChannelPlot):
     index_screen = Property(depends_on='index_data')
     value_data = Property(depends_on='reject_threshold, '
                           'source.added, source.updated')
-    value_screen = Property(depends_on='value_data')
 
     reject_threshold = Float(np.inf)
     update_rate = Int(5)
@@ -25,30 +27,26 @@ class EpochChannelPlot(BaseChannelPlot):
         return np.arange(self.source.epoch_size)/self.source.fs
 
     @cached_property
-    def _get_index_screen(self):
-        return self.index_mapper.map_screen(self.index_data)
-
-    @cached_property
     def _get_value_data(self):
         return self.source.get_average()
 
-    @cached_property
-    def _get_value_screen(self):
-        return self.value_mapper.map_screen(self.value_data)
-
     def _draw_plot(self, gc, view_bounds=None, mode="normal"):
-        self._render(gc)
+        if not self._screen_cache_valid:
+            s_index = self.index_mapper.map_screen(self.index_data)
+            s_value = self.value_mapper.map_screen(self.value_data)
+            self._screen_cache = s_index, s_value
+            self._screen_cache_valid = True
+        self._render(gc, self._screen_cache)
 
-    def _render(self, gc):
-        if len(self.value_screen) == 0:
+    def _render(self, gc, points):
+        if len(points[0]) == 0:
             return
         with gc:
-            print 'updating'
             gc.clip_to_rect(self.x, self.y, self.width, self.height)
             gc.set_stroke_color(self.line_color_)
             gc.set_line_width(self.line_width)
             gc.begin_path()
-            gc.lines(np.column_stack((self.index_screen, self.value_screen)))
+            gc.lines(np.column_stack(points))
             gc.stroke_path()
             gc.set_stroke_color(self.line_color_)
             gc.set_line_width(self.line_width)
