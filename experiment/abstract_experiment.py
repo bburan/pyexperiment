91c5f38f7f04ea452799d54e1d983c1ef9240636
Brad Buran <buran@ohsu.edu>
2014-10-22 17:18:30 -0700

    ENH: Add current context view to AbstractExperiment

--- experiment/abstract_experiment.py
+++ experiment/abstract_experiment.py
@@ -1,7 +1,33 @@
 from traits.api import HasTraits, Instance
+from traitsui.api import VGroup, Item, TabularEditor
+from traitsui.tabular_adapter import TabularAdapter
+
+
+class ContextAdapter(TabularAdapter):
+
+    columns = ['Parameter', 'Value', 'Variable']
+
+    def get_image(self, obj, trait, row, column):
+        if column == 0 and self.item[-2]:
+            return '@icons:tuple_node'
+
+    def get_width(self, obj, trait, column):
+        return 100
+
+    def get_bg_color(self, obj, trait, row, column=0):
+        return '#98FB98' if self.item[-1] else '#FFFFFF'
+
+
+context_editor = TabularEditor(adapter=ContextAdapter(), editable=False)
 
 
 class AbstractExperiment(HasTraits):
 
     paradigm = Instance('experiment.AbstractParadigm', ())
     data = Instance('experiment.AbstractData', ())
+
+    context_group = VGroup(
+        Item('handler.current_context_list', editor=context_editor),
+        show_labels=False,
+        label='Current Context',
+    )
